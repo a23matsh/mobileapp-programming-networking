4d6cf8bf667213e60b4bb1ef0fedf21501b6370f
a23matsh <a23matsh@student.his.se>
2024-04-25 14:45:54 +0200

    created mountains class

---
+++ app/src/main/java/Mountains.java
@@ -0,0 +1,22 @@
+public class Mountains {
+    private String name;
+    private int height;
+
+    public Mountains (String name, int height) {
+        this.name = name;
+        this.height = height;
+}
+
+    public String getName() {
+        return name;
+    }
+    public int getHeight() {
+        return height;
+    }
+
+
+
+
+
+}
+
--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import android.util.Log;
 
 
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,15 +17,25 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
     private final String JSON_FILE = "mountains.json";
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new JsonFile(this, this).execute(JSON_FILE);
-/*
         recyclerView = findViewById(R.id.recycler_view); // Make sure you have a RecyclerView with this ID in your layout
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        /*
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(RecyclerViewItem item) {
+                Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });*/
+/*
 
         ArrayList<RecyclerViewItem> items = new ArrayList<>(Arrays.asList(
                 new RecyclerViewItem("Matterhorn"),
@@ -33,22 +44,13 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     }
 */
 
+        new JsonFile(this, this).execute(JSON_FILE);
     }
+
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
     }
-
-
-/*
-    RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
-        @Override
-        public void onClick(RecyclerViewItem item) {
-            Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
-        }
-    });
-*/
-
 
 }
 
