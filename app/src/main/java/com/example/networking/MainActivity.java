edc55d79c62f19a0773ba2ca8ca6329f9c44a748
a23matsh <a23matsh@student.his.se>
2024-04-25 18:10:12 +0200

    allowed internet access for the app

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -9,19 +9,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
-    private final String JSON_URL = "HTTPS_URL_TO_JSON_DATA_CHANGE_THIS_URL";
+    private final String JSON_URL = "https://mobprog.webug.se/json-api?login=brom";
     private final String JSON_FILE = "mountains.json";
 
     private RecyclerView recyclerView;
     private RecyclerViewAdapter adapter;
     private ArrayList<RecyclerViewItem> items;
+
+    private List<Mountain> listOfMountains;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,21 +53,19 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         });
 
         recyclerView.setAdapter(adapter);
-/*
-        ArrayList<RecyclerViewItem> items = new ArrayList<>(Arrays.asList(
-                new RecyclerViewItem("Matterhorn"),
-                new RecyclerViewItem("Mont Blanc"),
-                new RecyclerViewItem("Denali")
-        ));
-*/
-
 
         new JsonFile(this, this).execute(JSON_FILE);
+        new JsonTask(this).execute(JSON_URL);
     }
 
     @Override
     public void onPostExecute(String json) {
         Log.d("MainActivity", json);
+        Gson gson = new Gson();
+
+        // Unmarshall JSON -> list of objects
+        Type type = new TypeToken<List<Mountain>>() {}.getType();
+        listOfMountains = gson.fromJson(json, type);
     }
 
 }
---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,19 @@
+package com.example.networking;
+
+
+import com.google.gson.annotations.SerializedName;
+
+public class Mountain {
+        private String ID;
+        private String name;
+        @SerializedName("cost")
+        private int feet;
+        @SerializedName("size")
+        private int meter;
+
+
+    }
+
+
+
+
